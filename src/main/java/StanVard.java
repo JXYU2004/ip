14c4cff29b0e79a5efd42b4ef526ecd9848ba55d
jixiang <yujixiang@outlook.com>
2026-08-21 13:22:11 +0800

    A-Enums

--- src/main/java/StanVard.java
+++ src/main/java/StanVard.java
@@ -9,6 +9,54 @@ public class StanVard {
     private static final String SEPARATOR = "____________________________________________________________";
 
     /**
+     * Represents the different commands supported by StanVard.
+     */
+    private enum CommandType {
+        LIST("list"),
+        MARK("mark"),
+        UNMARK("unmark"),
+        DELETE("delete"),
+        TODO("todo"),
+        DEADLINE("deadline"),
+        EVENT("event");
+
+        private final String keyword;
+
+        CommandType(String keyword) {
+            this.keyword = keyword;
+        }
+
+        /**
+         * Returns the keyword associated with this command type.
+         *
+         * @return command keyword
+         */
+        public String getKeyword() {
+            return keyword;
+        }
+
+        /**
+         * Determines the command type from the user's input.
+         *
+         * @param command trimmed command entered by the user
+         * @return the corresponding command type
+         * @throws StanVardException if the command is unknown
+         */
+        public static CommandType fromCommand(String command) throws StanVardException {
+            for (CommandType commandType : CommandType.values()) {
+                if (command.equals(commandType.keyword)
+                        || command.startsWith(commandType.keyword + " ")) {
+                    return commandType;
+                }
+            }
+
+            throw new StanVardException(
+                    "OOPS!!! I'm sorry, but I don't know what that means :-("
+            );
+        }
+    }
+
+    /**
      * Runs the chatbot's greeting, task command loop, and farewell sequence.
      *
      * @param args command-line arguments, which are not used
@@ -28,17 +76,22 @@ public class StanVard {
 
         Scanner scanner = new Scanner(System.in);
         List<Task> tasks = new ArrayList<>();
+
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
+
             if (command.equals("bye")) {
                 break;
             }
+
             System.out.println(SEPARATOR);
+
             try {
                 handleCommand(command, tasks);
             } catch (StanVardException exception) {
                 System.out.println(exception.getMessage());
             }
+
             System.out.println(SEPARATOR);
         }
 
@@ -67,77 +120,150 @@ public class StanVard {
      */
     private static void handleCommand(String command, List<Task> tasks) throws StanVardException {
         String trimmedCommand = command.trim();
-        if (trimmedCommand.equals("list")) {
-            printTaskList(tasks);
-        } else if (isCommand(trimmedCommand, "mark")) {
-            int taskIndex = parseTaskIndex(trimmedCommand, "mark", tasks);
-            tasks.get(taskIndex).markAsDone();
-            System.out.println("Nice! I've marked this task as done:");
-            System.out.println("  " + tasks.get(taskIndex));
-        } else if (isCommand(trimmedCommand, "unmark")) {
-            int taskIndex = parseTaskIndex(trimmedCommand, "unmark", tasks);
-            tasks.get(taskIndex).markAsNotDone();
-            System.out.println("OK, I've marked this task as not done yet:");
-            System.out.println("  " + tasks.get(taskIndex));
-        } else if (isCommand(trimmedCommand, "delete")) {
-            int taskIndex = parseTaskIndex(trimmedCommand, "delete", tasks);
-            Task deletedTask = tasks.remove(taskIndex);
-            printDeletedTask(deletedTask, tasks);
-        } else if (isCommand(trimmedCommand, "todo")) {
-            String description = trimmedCommand.substring("todo".length()).trim();
-            if (description.isEmpty()) {
-                throw new StanVardException("OOPS!!! The description of a todo cannot be empty.");
-            }
-            addTask(new Todo(description), tasks);
-        } else if (isCommand(trimmedCommand, "deadline")) {
-            String details = trimmedCommand.substring("deadline".length()).trim();
-            int byIndex = details.indexOf("/by");
-            if (byIndex < 0) {
-                throw new StanVardException("OOPS!!! A deadline must include /by followed by a date/time.");
-            }
-            String description = details.substring(0, byIndex).trim();
-            String by = details.substring(byIndex + "/by".length()).trim();
-            if (description.isEmpty()) {
-                throw new StanVardException("OOPS!!! The description of a deadline cannot be empty.");
-            }
-            if (by.isEmpty()) {
-                throw new StanVardException("OOPS!!! The deadline time cannot be empty.");
-            }
-            addTask(new Deadline(description, by), tasks);
-        } else if (isCommand(trimmedCommand, "event")) {
-            String details = trimmedCommand.substring("event".length()).trim();
-            int fromIndex = details.indexOf("/from");
-            int toIndex = details.indexOf("/to");
-            if (fromIndex < 0 || toIndex < 0 || toIndex < fromIndex) {
-                throw new StanVardException("OOPS!!! An event must include /from and /to times.");
-            }
-            String description = details.substring(0, fromIndex).trim();
-            String from = details.substring(fromIndex + "/from".length(), toIndex).trim();
-            String to = details.substring(toIndex + "/to".length()).trim();
-            if (description.isEmpty()) {
-                throw new StanVardException("OOPS!!! The description of an event cannot be empty.");
-            }
-            if (from.isEmpty()) {
-                throw new StanVardException("OOPS!!! The event start time cannot be empty.");
-            }
-            if (to.isEmpty()) {
-                throw new StanVardException("OOPS!!! The event end time cannot be empty.");
-            }
-            addTask(new Event(description, from, to), tasks);
-        } else {
-            throw new StanVardException("OOPS!!! I'm sorry, but I don't know what that means :-(");
-        }
-    }
+        CommandType commandType = CommandType.fromCommand(trimmedCommand);
 
-    /**
-     * Checks whether the entered text is a command with an optional argument.
-     *
-     * @param command trimmed command entered by the user
-     * @param keyword command keyword to check
-     * @return whether the command matches the keyword
-     */
-    private static boolean isCommand(String command, String keyword) {
-        return command.equals(keyword) || command.startsWith(keyword + " ");
+        switch (commandType) {
+            case LIST:
+                printTaskList(tasks);
+                break;
+
+            case MARK:
+                int markIndex = parseTaskIndex(
+                        trimmedCommand,
+                        commandType.getKeyword(),
+                        tasks
+                );
+                tasks.get(markIndex).markAsDone();
+
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + tasks.get(markIndex));
+                break;
+
+            case UNMARK:
+                int unmarkIndex = parseTaskIndex(
+                        trimmedCommand,
+                        commandType.getKeyword(),
+                        tasks
+                );
+                tasks.get(unmarkIndex).markAsNotDone();
+
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + tasks.get(unmarkIndex));
+                break;
+
+            case DELETE:
+                int deleteIndex = parseTaskIndex(
+                        trimmedCommand,
+                        commandType.getKeyword(),
+                        tasks
+                );
+                Task deletedTask = tasks.remove(deleteIndex);
+
+                printDeletedTask(deletedTask, tasks);
+                break;
+
+            case TODO:
+                String todoDescription = trimmedCommand
+                        .substring(commandType.getKeyword().length())
+                        .trim();
+
+                if (todoDescription.isEmpty()) {
+                    throw new StanVardException(
+                            "OOPS!!! The description of a todo cannot be empty."
+                    );
+                }
+
+                addTask(new Todo(todoDescription), tasks);
+                break;
+
+            case DEADLINE:
+                String deadlineDetails = trimmedCommand
+                        .substring(commandType.getKeyword().length())
+                        .trim();
+
+                int byIndex = deadlineDetails.indexOf("/by");
+
+                if (byIndex < 0) {
+                    throw new StanVardException(
+                            "OOPS!!! A deadline must include /by followed by a date/time."
+                    );
+                }
+
+                String deadlineDescription = deadlineDetails
+                        .substring(0, byIndex)
+                        .trim();
+
+                String by = deadlineDetails
+                        .substring(byIndex + "/by".length())
+                        .trim();
+
+                if (deadlineDescription.isEmpty()) {
+                    throw new StanVardException(
+                            "OOPS!!! The description of a deadline cannot be empty."
+                    );
+                }
+
+                if (by.isEmpty()) {
+                    throw new StanVardException(
+                            "OOPS!!! The deadline time cannot be empty."
+                    );
+                }
+
+                addTask(new Deadline(deadlineDescription, by), tasks);
+                break;
+
+            case EVENT:
+                String eventDetails = trimmedCommand
+                        .substring(commandType.getKeyword().length())
+                        .trim();
+
+                int fromIndex = eventDetails.indexOf("/from");
+                int toIndex = eventDetails.indexOf("/to");
+
+                if (fromIndex < 0 || toIndex < 0 || toIndex < fromIndex) {
+                    throw new StanVardException(
+                            "OOPS!!! An event must include /from and /to times."
+                    );
+                }
+
+                String eventDescription = eventDetails
+                        .substring(0, fromIndex)
+                        .trim();
+
+                String from = eventDetails
+                        .substring(fromIndex + "/from".length(), toIndex)
+                        .trim();
+
+                String to = eventDetails
+                        .substring(toIndex + "/to".length())
+                        .trim();
+
+                if (eventDescription.isEmpty()) {
+                    throw new StanVardException(
+                            "OOPS!!! The description of an event cannot be empty."
+                    );
+                }
+
+                if (from.isEmpty()) {
+                    throw new StanVardException(
+                            "OOPS!!! The event start time cannot be empty."
+                    );
+                }
+
+                if (to.isEmpty()) {
+                    throw new StanVardException(
+                            "OOPS!!! The event end time cannot be empty."
+                    );
+                }
+
+                addTask(new Event(eventDescription, from, to), tasks);
+                break;
+
+            default:
+                throw new StanVardException(
+                        "OOPS!!! I'm sorry, but I don't know what that means :-("
+                );
+        }
     }
 
     /**
@@ -145,34 +271,52 @@ public class StanVard {
      *
      * @param command trimmed command entered by the user
      * @param keyword command keyword to remove
+     * @param tasks tasks currently stored by the chatbot
      * @return the zero-based task array index
      * @throws StanVardException if the task number is missing, invalid, or out of range
      */
-    private static int parseTaskIndex(String command, String keyword, List<Task> tasks) throws StanVardException {
+    private static int parseTaskIndex(
+            String command,
+            String keyword,
+            List<Task> tasks) throws StanVardException {
+
         String numberText = command.substring(keyword.length()).trim();
+
         if (numberText.isEmpty()) {
-            throw new StanVardException("OOPS!!! The task number to " + keyword + " cannot be empty.");
+            throw new StanVardException(
+                    "OOPS!!! The task number to " + keyword + " cannot be empty."
+            );
         }
 
         int taskNumber;
+
         try {
             taskNumber = Integer.parseInt(numberText);
         } catch (NumberFormatException exception) {
-            throw new StanVardException("OOPS!!! The task number must be a positive integer.");
+            throw new StanVardException(
+                    "OOPS!!! The task number must be a positive integer."
+            );
         }
+
         if (taskNumber <= 0) {
-            throw new StanVardException("OOPS!!! The task number must be a positive integer.");
+            throw new StanVardException(
+                    "OOPS!!! The task number must be a positive integer."
+            );
         }
+
         if (taskNumber > tasks.size()) {
-            throw new StanVardException("OOPS!!! The task number is out of range.");
+            throw new StanVardException(
+                    "OOPS!!! The task number is out of range."
+            );
         }
+
         return taskNumber - 1;
     }
 
     /**
      * Adds a validated task and displays its confirmation.
      *
-     * @param task task to add
+     * @param task the task to add
      * @param tasks task storage list
      */
     private static void addTask(Task task, List<Task> tasks) {
@@ -184,7 +328,7 @@ public class StanVard {
      * Prints the confirmation shown after deleting a task.
      *
      * @param task deleted task
-     * @param tasks task storage list after deletion
+     * @param tasks tasks currently stored after deletion
      */
     private static void printDeletedTask(Task task, List<Task> tasks) {
         System.out.println("Noted. I've removed this task:");
@@ -195,10 +339,11 @@ public class StanVard {
     /**
      * Prints all tasks in their stored order.
      *
-     * @param tasks task storage list
+     * @param tasks tasks currently stored by the chatbot
      */
     private static void printTaskList(List<Task> tasks) {
         System.out.println("Here are the tasks in your list:");
+
         for (int index = 0; index < tasks.size(); index++) {
             System.out.println((index + 1) + "." + tasks.get(index));
         }
